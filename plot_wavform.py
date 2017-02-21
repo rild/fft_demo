381d83379b682a9bb702d90169741eff9f8c8f24
Iida Taiki <rild3178ata@gmail.com>
2017-02-21 17:53:21 +0900

    start and goal

--- plot_wavform.py
+++ plot_wavform.py
@@ -16,6 +16,26 @@ def plot_waveform(waveform, sampling_rate):
     pylab.ylim([-1, 1])
     pylab.show()
 
+def plt_wav(waveform, sampling_rate, start_time, goal_time):
+    times = numpy.arange(float(len(waveform))) / sampling_rate # 各サンプルの時刻
+    # new_times = []
+    # frames = []
+    # # ここ for 文ではない処理に変えたい..
+    # for i in range(0, len(times)):
+    #     if times[i] > start_time and times[i] < goal_time:
+    #         new_times.append(times[i])
+    #         frames.append(waveform[i])
+
+    xlimit =  len(waveform) / sampling_rate
+
+    pylab.plot(times, waveform) # pair of x- and y-coordinate lists/arrays
+    pylab.title("Waveform")
+    pylab.xlabel("Time[sec]")
+    pylab.ylabel("Amplitude")
+    pylab.xlim([start_time, goal_time])
+    pylab.ylim([-1, 1])
+    pylab.show()
+
 if __name__ == "__main__":
     argv = sys.argv
     if len(argv) == 1:
@@ -26,6 +46,11 @@ if __name__ == "__main__":
     sampling_rate, waveform = scipy.io.wavfile.read(filename)
     # WAVファイルのフォーマットが符号あり16bit整数であることを仮定する
     # 波形が -1.0 ~ 1.0 の範囲に収まるように正規化を行う
+    waveform = waveform / 32768.0 # pcm?
 
-waveform = waveform / 32768.0 # pcm?
-plot_waveform(waveform, sampling_rate)
+    if len(argv) == 4:
+        start = float(argv[2])
+        goal = float(argv[3])
+        plt_wav(waveform, sampling_rate, start, goal)
+    else:
+        plot_waveform(waveform, sampling_rate)

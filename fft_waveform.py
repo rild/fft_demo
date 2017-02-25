7d86354ebdf179f6e9382a1cd47b9486e05d6258
Iida Taiki <rild3178ata@gmail.com>
2017-02-25 11:16:53 +0900

    create fft

---
+++ fft_waveform.py
@@ -0,0 +1,16 @@
+import numpy as np
+import sys
+
+def fft_wav(waveform, sampling_rate, start_time, goal_time):
+    print "fft start"
+
+def dft_wav(waveform, sampling_rate, start_time, goal_time):
+    print "dft start"
+
+if __name__ == "__main__":
+    argv = sys.argv
+    argc = len(argv)
+
+    if argc == 1:
+        print "inlavid argument"
+        exit()
